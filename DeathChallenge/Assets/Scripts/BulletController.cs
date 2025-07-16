f34ddcab2f8f51c707bfc8f983a26ed5f2c069be
Nguyễn Song Toàn <toannsse183104@fpt.edu.vn>
2025-07-16 22:25:46 +0700

    fix : sound name

--- DeathChallenge/Assets/Scripts/BulletController.cs
+++ DeathChallenge/Assets/Scripts/BulletController.cs
@@ -33,7 +33,8 @@ public class Projectile : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             // Phát âm thanh khi đạn trúng người chơi
-            AudioManager.instance.Play("Bullet_Hit");
+            
+            AudioManager.instance.Play("Ghost_Hit");
 
             Debug.Log("Player hit the player!");
             PlayerHealth playerHealth = other.GetComponent<PlayerHealth>();
